2e979c196377a8b6e9cec3ccf192f35054c7f5cd
JoonHyeok Han <joonhyeok.han@gmail.com>
2025-04-05 15:31:56 +0900

    feat: WalletMultiButton 클래스 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             함께 성장하는 플랫폼
           </p>
           <div className="flex justify-center">
-            <WalletMultiButton className="!bg-blue-600 hover:!bg-blue-700 !text-white !font-bold !py-2 !px-4 !rounded-lg !shadow-lg hover:!shadow-xl transition-all duration-200" />
+            <WalletMultiButton />
           </div>
         </div>
 
